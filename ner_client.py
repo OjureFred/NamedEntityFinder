6087d1aecfbce1682555314806318fec568c45a7
Fredrick Ojure <fredojure@gmail.com>
2022-10-12 15:54:16 +0300

    6. Added tests for api and index page

--- ner_client.py
+++ ner_client.py
@@ -1,15 +1,14 @@
-import spacy
-
-
 class NamedEntityClient:
-    def __init__(self, model):
+    def __init__(self, model, displacy):
         self.model = model
+        self.displacy = displacy
 
     def get_ents(self, sentence):
         doc = self.model(sentence)
-        entities = [{'ent': ent.text, 'label': self.map_label(ent.label_)}
-                    for ent in doc.ents]
-        return {'ents': entities, 'html': ''}
+        html = self.displacy.render(doc, style="ent")
+        entities = [{'ent': ent.text, 'label': self.map_label(
+            ent.label_)} for ent in doc.ents]
+        return {'ents': entities, 'html': html}
 
     @staticmethod
     def map_label(label):
@@ -17,8 +16,8 @@ class NamedEntityClient:
             'PERSON': 'Person',
             'NORP': 'Group',
             'LOC': 'Location',
-            'ASL': 'Language',
-            'Australia': 'GPE'
+            'GPE': 'Location',
+            'LANGUAGE': 'Language'
         }
 
         return label_map.get(label)
